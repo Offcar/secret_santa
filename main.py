f9eaa3016699cfe34a6ca7762c4b46fed3a9b170
Offcar <andresortizg8@gmail.com>
2022-12-20 20:19:44 -0300

    Libreria lenguajes

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from random import shuffle
 
+langSelected = 'en'
+lang = {
+    'en': 'Hello %s, you must give a gift to %s',
+    'es': 'Hola %s, debes dar un regalo a %s',
+}
+
 # Read filename to gather names of participants
 with open('nameList.txt', 'r') as file:
     names = [line.strip() for line in file]
@@ -12,5 +18,5 @@ names.append(names[0])
 
 for i in range(0, len(names)-1):
     f = open(names[i]+'.txt', 'w')
-    f.write('Hola '+names[i]+', tu amigo secreto es '+names[i+1])
+    f.write(lang[langSelected] % (names[i], names[i+1]))
     f.close()
